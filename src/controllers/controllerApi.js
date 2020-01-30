8a555ec20f0f73be0589d075e547616e3c89eae1
JOHN WANDERSON VIANA DA LUZ <FR041743@catolica.ubec>
2020-01-30 19:10:16 -0300

    atualizacao

--- src/controllers/controllerApi.js
+++ src/controllers/controllerApi.js
@@ -1,7 +1,11 @@
 const apiGit = require('../services/serviceGit');
 const apiFace = require('../services/serviceFace');
 
+const _ = require('lodash')
+
+
 module.exports = async function get(req,res, next){
+    let repoDefault = [];
 
     try {
         
@@ -11,11 +15,30 @@ module.exports = async function get(req,res, next){
             Authorizathion: "token d88b002b8e05cd582add1fb5fac730b1cafd3aa1"
         })
 
-        res.json({ github_profile: {
+        let { data: repos } = await apiGit.get(`/users/${name}/repos`)
+
+        repos.map(r => {
+            repoDefault.push({
+                size: r.size,
+                name: r.name,
+                url: r.url,
+            })
+        })
+
+
+        repoDefault.sort((a, b) => {
+            return b.size - a.size
+        })
+
+        const repoArr = _.chunk(repoDefault, 3)
+
+
+        return res.json({ github_profile: {
             name: data.name,
             url: data.url,
             bio: data.bio,
             company: data.company,
+            repositorios: [...repoArr[0]]
         }})
 
 
